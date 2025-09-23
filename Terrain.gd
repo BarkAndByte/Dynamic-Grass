496a7d52f4b38255cfcdd2d5ebb99fed47111fa9
BarkAndByte <chrissibleyer@googlemail.com>
2025-09-23 08:35:50 +0200

    Attempting to apply all parameters in the shader

--- Terrain.gd
+++ Terrain.gd
@@ -1,7 +1,10 @@
 @tool
 extends StaticBody3D
 
-@export_tool_button("Generate Grass", "Callable") var action = createGrass
+
+@export_group("Grass Distribution")
+
+@export_tool_button("Recompute Grass", "Callable") var action = createGrass
 
 # Parameters for the grass
 # How far apart the blades of grass are
@@ -20,16 +23,27 @@ var next_blade_to_place : int = 0
 
 #### Variables for creating the terrain 
 @export var dimensions : Vector2i = Vector2i(1,1)
-@export var heightmap : Image
-@export var grassmap : Image
-@export var grass_noise : Image
 
+
+@export_group("Terrain Properties")
+## Height Factor, that is multiplied with heightmap for blade position. Should match Terrain
+@export var terrain_height_scale : float = 1.0
+
+@export_group("Textures")
+## Grayscale Image describing height of terrain at that position
+@export var texture_height : Texture2D
+## Grayscale Image describing height of grass at that postion
+@export var texture_size : Texture2D
+## Grayscale Image describing color change by scrolling Wind 
+@export var texture_wind : Texture2D
+## Grayscale Image describing color change by static pattern
+@export var texture_pattern : Texture2D
 
 
 
 func createTerrain() -> void:
+	pass
 	# Resize Images 
-	heightmap.resize(dimensions.x,dimensions.y,Image.INTERPOLATE_BILINEAR)
 	
 	# Build Collision
 	
@@ -40,6 +54,27 @@ func createTerrain() -> void:
 
 
 func createGrass() -> void:
+	# Place Grass in efficient Pattern around Center
+	
+	# Create truly random noise image for rotation and offset
+	var size : Vector2i = Vector2i(texture_height.get_size())
+	var offset_image : Image = Image.create_empty(size.x,size.y,false,Image.FORMAT_RGB8)
+	for i in range(size.x):
+		for j in range(size.y):
+			offset_image.set_pixel(i,j,Color(randf(),randf(),randf()))
+	
+	var texture_offset : ImageTexture = ImageTexture.create_from_image(offset_image)
+	# Possible Todo: Combine mulötiple Channels into single texture
+	# Set Values in Shader
+	var material : Material = $MultiMeshGrass.material_override
+	material.set_shader_parameter("height_texture",texture_height)
+	material.set_shader_parameter("size_texture",texture_size)
+	material.set_shader_parameter("wind_texture",texture_wind)
+	material.set_shader_parameter("pattern_texture",texture_pattern)
+	material.set_shader_parameter("offset_texture",texture_offset)
+	
+	
+	
 	var x_num  : int = floor(dimensions.x / placement_distance.x)
 	var y_num : int = floor(dimensions.y / placement_distance.y)
 	number_of_blades =  x_num* y_num 
@@ -55,15 +90,24 @@ func createGrass() -> void:
 	while i < x_num:
 		var j : int = 0
 		while j < y_num:
-			var p:Vector3 = Vector3(i*placement_distance.x +offsetx + (random_offset*(randf() - 0.5 )* 2),0,j * placement_distance.x+offsetz+ (random_offset*(randf() - 0.5 )* 2))
+			#var p:Vector3 = Vector3(i*placement_distance.x +offsetx + (random_offset*(randf() - 0.5 )* 2),0,j * placement_distance.x+offsetz+ (random_offset*(randf() - 0.5 )* 2))
+			var p:Vector3 = Vector3(i*placement_distance.x +offsetx ,0,j * placement_distance.x+offsetz) 
 			var ground_uv : Vector2 = Vector2((p.x - offsetx) / dimensions.x , (p.z - offsetz) / dimensions.y)# Vector2(float(i) / x_num , float(j) / y_num) #
-			multimesh.set_instance_transform(next_blade_to_place,Transform3D(Basis(Vector3.UP,randf()*2*PI).scaled(base_scale),p))
+			multimesh.set_instance_transform(next_blade_to_place,Transform3D(Basis(Vector3.UP,0.0).scaled(base_scale),p)) #randf()*2*PI
 			multimesh.set_instance_custom_data(next_blade_to_place,Color(ground_uv.x,ground_uv.y,0,0))
 			next_blade_to_place += 1
 			
 			j += 1
 		i += 1
+		
+
+func updateGrass() -> void:
+	pass
+	# Move entire Node By some fixed offset
 	
+	# Possibly update Textures for the Shader
+	
+	 
 
 
 
